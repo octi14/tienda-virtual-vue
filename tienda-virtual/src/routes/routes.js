0e6ab79b63087c8146a34bdf3bfc4d53ee321de3
octi14 <gonzalezoctavio5@gmail.com>
2021-08-21 04:54:41 -0300

    Routes funcionando

--- tienda-virtual/src/routes/routes.js
+++ tienda-virtual/src/routes/routes.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createWebHistory, createRouter } from "vue-router"
 import HelloWorld from '../components/HelloWorld.vue'
-
-Vue.use(VueRouter)
+import Ofertas from '../components/Ofertas.vue'
+import Publicar from '../components/Publicar.vue'
+import Explorar from '../components/Explorar.vue'
 
 const routes = [
   {
@@ -11,48 +11,25 @@ const routes = [
     component: HelloWorld
   },
   {
+    path: '/explorar',
+    name: 'Explorar',
+    component: Explorar
+  },
+  {
     path: '/ofertas',
     name: 'Ofertas',
-    component: () => import('../components/Ofertas.vue')
+    component: Ofertas
   },
   {
     path: '/publicar',
     name: 'Publicar',
-    component: () => import('../components/Publicar.vue')
-  },
-  {
-    path: '/contacto',
-    name: 'Contacto',
-    component: () => import('../components/Contacto.vue')
-  },
+    component: Publicar
+  }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/', '/ofertas', '/publicar', '/contacto']
-  const authRequired = publicPages.reduce( (authReq, page) => { 
-    return authReq && !to.path.includes(page)
-  }, true)
-  const tryingToLogin = to.path === '/login';
-  const tryingToRegister = to.path === '/register';
-  const loggedIn = localStorage.getItem('user')
-
-  if (authRequired && !loggedIn)
-    return next('/login')
-
-  if (tryingToLogin && loggedIn)
-    return next('/')
-
-  if (tryingToRegister && loggedIn)
-    return next('/')
-
-  next()
+const router = createRouter({
+    history: createWebHistory(),
+    routes
 })
 
 export default router
